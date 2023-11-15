ddc358714ec514f44b545a6ce0625572fca1fe2a
Helder45 <helder.pereira@estudante.ifms.edu.br>
2023-11-15 16:40:25 -0400

    altering stuff

--- js/carrinho.js
+++ js/carrinho.js
@@ -1,9 +1,46 @@
-// Just creating
+const checkOutBtn = document.getElementById("checkOutBtn");
 
-const finalizarBtn = document.getElementById("finalizarBtn");
+window.onload = deleteExecution;
 
-finalizarBtn.addEventListener("click", (page) => {
-    page.preventDefault();
+const itemsTotal = document.getElementById("itemsTotal");
+const products = document.getElementsByClassName("card");
+deleteBtns = document.getElementsByClassName("deleteBtn");
 
-    alert("Processando... Você será redirecionado em instantes!");
+itemsTotal.addEventListener("load", () => {
+  //por localStorage o LocalStorage que vai ser responsavel por navegar dados entre paginas
+  //isso mesmo
+
+  //certo então quando clicar em comprar vou estar colocando no local storage?
+  //ata blz
+  //vou estar fazendo aqui então
+  //blz, bom trabalho meu mano
+  // deixa eu comm
+  mitar primeiro
+});
+
+function deleteExecution() {
+  for (i = 0; i < deleteBtns.length; i++) {
+    (function (index) {
+      deleteBtns[i].addEventListener("click", function () {
+        removeCard(index);
+      });
+    })(i);
+  }
+}
+
+function removeCard(element) {
+  console.log(element);
+  deleteBtns[element].parentNode.parentNode.remove();
+
+  for (i = 0; i < deleteBtns.length; i++) {
+    objclone = deleteBtns[i].cloneNode(true);
+    deleteBtns[i].parentNode.replaceChild(objclone, deleteBtns[i]);
+  }
+  deleteExecution();
+}
+
+checkOutBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  alert("Processando... Você será redirecionado em instantes!");
 });
--- js/favoritos.js
+++ js/favoritos.js
@@ -1,1 +1,24 @@
-// Just creating
+window.onload = deleteExecution;
+
+deleteBtns = document.getElementsByClassName("deleteBtn");
+
+function deleteExecution() {
+  for (i = 0; i < deleteBtns.length; i++) {
+    (function (index) {
+      deleteBtns[i].addEventListener("click", function () {
+        removeCard(index);
+      });
+    })(i);
+  }
+}
+
+function removeCard(element) {
+  console.log(element);
+  deleteBtns[element].parentNode.parentNode.parentNode.remove();
+
+  for (i = 0; i < deleteBtns.length; i++) {
+    objclone = deleteBtns[i].cloneNode(true);
+    deleteBtns[i].parentNode.replaceChild(objclone, deleteBtns[i]);
+  }
+  deleteExecution();
+}
