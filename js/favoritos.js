6a0716a082936c9f5c7fc7d3e5407ba40bce7009
Helder45 <helder.pereira@estudante.ifms.edu.br>
2023-11-16 16:56:41 -0400

    fav working too

--- js/favoritos.js
+++ js/favoritos.js
@@ -1,6 +1,10 @@
 window.onload = deleteExecution;
 
-deleteBtns = document.getElementsByClassName("deleteBtn");
+let deleteBtns = document.getElementsByClassName("deleteBtn");
+
+document
+  .getElementById("principal")
+  .addEventListener("load", pegarLocalStorage());
 
 function deleteExecution() {
   for (i = 0; i < deleteBtns.length; i++) {
@@ -21,4 +25,46 @@ function removeCard(element) {
     deleteBtns[i].parentNode.replaceChild(objclone, deleteBtns[i]);
   }
   deleteExecution();
+}
+
+function pegarLocalStorage() {
+  //Mudar estrutura HTML, para grid
+  const coluna = document.getElementById("coluna");
+
+  let itemsLocalStorage = JSON.parse(localStorage.getItem("favorito"));
+
+  if (itemsLocalStorage === null) {
+    alert("Não há itens nos favoritos!");
+  } else {
+    for (let items = 0; items < itemsLocalStorage.length; items++) {
+      //creating elements
+      let cardDiv = document.createElement("div");
+      let cardBodyDiv = document.createElement("div");
+      let cardTitle = document.createElement("h5");
+      let cardText = document.createElement("p");
+      let cardBtn = document.createElement("button");
+
+      //setting attributes
+      cardDiv.setAttribute("class", "card my-2");
+
+      cardBodyDiv.setAttribute("class", "card-body");
+
+      cardTitle.setAttribute("class", "card-title");
+
+      cardText.setAttribute("class", "card-text");
+
+      cardBtn.setAttribute("class", "btn btn-danger deleteBtn");
+
+      cardBtn.textContent = "Excluir";
+
+      cardTitle.textContent = itemsLocalStorage[items].nome;
+      cardText.textContent = itemsLocalStorage[items].preco;
+
+      cardBodyDiv.appendChild(cardTitle);
+      cardBodyDiv.appendChild(cardText);
+      cardBodyDiv.appendChild(cardBtn);
+      cardDiv.appendChild(cardBodyDiv);
+      coluna.appendChild(cardDiv);
+    }
+  }
 }
